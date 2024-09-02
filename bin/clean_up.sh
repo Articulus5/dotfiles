6979b5407810f6a3d1f9402e3328b76cc1c7a1ad
Stanley Tse <stanleytse@arcares.net>
2024-09-01 23:27:18 -0700

    Modified script to use the user's home directory.

--- bin/clean_up.sh
+++ bin/clean_up.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-rm -rf /home/stse/.cache/vim                                   2>/dev/null
-rm     /home/stse/.local/share/okular/docdata/*.xml            2>/dev/null
-rm     /home/stse/.local/share/recently-used.xbel              2>/dev/null
-rm     /home/stse/{.bash_history,.lesshst,.viminfo,.wget-hsts} 2>/dev/null
+rm -rf /home/$USER/.cache/vim                                   2>/dev/null
+rm     /home/$USER/.local/share/okular/docdata/*.xml            2>/dev/null
+rm     /home/$USER/.local/share/recently-used.xbel              2>/dev/null
+rm     /home/$USER/{.bash_history,.lesshst,.viminfo,.wget-hsts} 2>/dev/null
+
 exit 0
