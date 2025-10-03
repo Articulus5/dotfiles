fcaf3336e954ee398691ac85b1c87b06c9cd3070
Stanley Tse <stanleytse@arcares.net>
2025-10-02 21:49:27 -0700

    Added additional history/log files to clean up.

--- bin/clean_up.sh
+++ bin/clean_up.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-rm -rf /home/$USER/.cache/vim                                   2>/dev/null
-rm     /home/$USER/.local/share/okular/docdata/*.xml            2>/dev/null
-rm     /home/$USER/.local/share/recently-used.xbel              2>/dev/null
-rm     /home/$USER/{.bash_history,.lesshst,.viminfo,.wget-hsts} 2>/dev/null
+rm -rf /home/$USER/.cache/vim                        2>/dev/null
+rm -rf /home/$USER/.ollama                           2>/dev/null
+rm     /home/$USER/.local/share/recently-used.xbel   2>/dev/null
+rm     /home/$USER/.local/share/okular/docdata/*.xml 2>/dev/null
+rm     /home/$USER/{.bash_history,.lesshst,.viminfo,.wget-hsts,.python_history} 2>/dev/null
 
 exit 0
